17fe505075effb6863bf6fa64df2188a2ea5bdcc
RasOtte <Rasmus.otterlind@gmail.com>
2022-04-17 18:40:41 +0200

    some work done on lobby

---
+++ Assets/Scrpits/CreateAndJoinRooms.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private TMP_InputField createInput;
+    [SerializeField] private TMP_InputField joinInput;
+
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+}
