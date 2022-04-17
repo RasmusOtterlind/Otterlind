7edc6f513f163300a48b287eb1e93b99e82c5ea5
mawza <oliver.otterlind@gmail.com>
2022-04-17 22:43:35 +0200

    absj

---
+++ Assets/Scrpits/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class GameManager : MonoBehaviour
+{
+
+    private bool debug = true;
+    private float timeSinceStart = 0f;
+
+    private float timeSinceCoin = 0f;
+    private float cooldownCoin = 0f;
+    private int amountOfCoins = 0;
+
+    private void DebugPrint(string debugString)
+    {
+        if (debug){
+            Debug.Log(debugString);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DebugPrint("We have begun bitch");
+        DebugPrint("Time is for server: " + PhotonNetwork.Time);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PhotonNetwork.IsMasterClient){
+            timeSinceStart += Time.deltaTime;
+            coinHandler();
+            timeSinceCoin += Time.deltaTime;
+
+            DebugPrint("Time for server: " + timeSinceStart);
+            if ((timeSinceStart % 100) == 0){
+                DebugPrint("Time for server: " + timeSinceStart);
+
+            }
+        }
+    }
+}
