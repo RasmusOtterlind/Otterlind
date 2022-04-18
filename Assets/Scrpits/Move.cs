a81fe720f2dff78edb314fc07fd9102c4a545327
RasOtte <Rasmus.otterlind@gmail.com>
2022-04-18 17:10:21 +0200

    a disease

--- Assets/Scrpits/Move.cs
+++ Assets/Scrpits/Move.cs
@@ -13,6 +13,10 @@ public class Move : MonoBehaviour
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private Transform groundChecker;
 
+    //camera
+    private float xRotation = 0;
+
+
     private bool firstJump = false;
     // Start is called before the first frame update
     private bool debug = true;
@@ -28,6 +32,7 @@ public class Move : MonoBehaviour
     {
         photonView = GetComponent<PhotonView>();
         rigidBody = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
 
     }
 
@@ -36,31 +41,47 @@ public class Move : MonoBehaviour
     {
         if (photonView.IsMine)
         {
-            forwardBackward = Input.GetAxisRaw("Vertical");
-
-            sideStep = Input.GetAxisRaw("Horizontal");
+            HandleMovement();
+            HandleRotation();
             
-            if (Input.GetButtonDown("Jump"))
-            {
-                if(Physics.CheckSphere(groundChecker.position, 0.2f, groundMask, QueryTriggerInteraction.Ignore))
-                {
-              
-                    rigidBody.AddForce(transform.up * 600);
-                    firstJump = true;
-                }
-                else if (firstJump)
-                {
-                    rigidBody.AddForce(transform.up * 600);
-                    firstJump = false;
-                }
-
-            }
-            transform.Rotate(0, sideStep * 80 * Time.deltaTime, 0, relativeTo: Space.World);
-            rigidBody.velocity = transform.forward * speed * forwardBackward + new Vector3(0, rigidBody.velocity.y, 0);
         }
         else
         { //Vi äger ej denna view
             return;
         }
+    }
+
+    private void HandleMovement()
+    {
+        forwardBackward = Input.GetAxisRaw("Vertical");
+
+        sideStep = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (Physics.CheckSphere(groundChecker.position, 0.2f, groundMask, QueryTriggerInteraction.Ignore))
+            {
+
+                rigidBody.AddForce(transform.up * 600);
+                firstJump = true;
+            }
+            else if (firstJump)
+            {
+                rigidBody.AddForce(transform.up * 600);
+                firstJump = false;
+            }
+
+        }
+        transform.Rotate(0, sideStep * 80 * Time.deltaTime, 0, relativeTo: Space.World);
+        rigidBody.velocity = transform.forward * speed * forwardBackward + new Vector3(0, rigidBody.velocity.y, 0);
+    }
+
+    private void HandleRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * 5;
+        float mouseY = Input.GetAxis("Mouse Y") * 5;
+        transform.Rotate(mouseY * 50 * Time.deltaTime, 0, 0, relativeTo: Space.World);
+        transform.Rotate(0, mouseX * 50 * Time.deltaTime, 0, relativeTo: Space.World);
+
     }
 }
