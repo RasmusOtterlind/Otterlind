1854bf0712b87f7198742ad8793b453e73af886a
RasOtte <Rasmus.otterlind@gmail.com>
2022-05-10 22:24:25 +0200

    added enemy spawner

--- Assets/Scrpits/GameManager.cs
+++ Assets/Scrpits/GameManager.cs
@@ -10,9 +10,12 @@ public class GameManager : MonoBehaviour
     private float timeSinceStart = 0f;
 
     private float timeSinceCoin = 0f;
-    [SerializeField]private float cooldownCoin = 10f;
+    [SerializeField] private float cooldownCoin = 10f;
+    private float timeSinceEnemy = 0f;
+    [SerializeField] private float cooldownEnemySpawn = 20f;
     private int amountOfCoins = 0;
     public GameObject[] powerUpPickupList;
+    public GameObject[] enemyList;
     private void DebugPrint(string debugString)
     {
         if (debug){
@@ -34,13 +37,20 @@ public class GameManager : MonoBehaviour
         if (PhotonNetwork.IsMasterClient){
             timeSinceStart += Time.deltaTime;
             timeSinceCoin += Time.deltaTime;
+            timeSinceEnemy += Time.deltaTime;
             DebugPrint("Time Since Coin: " + timeSinceCoin);
 
             if (timeSinceCoin  > cooldownCoin){
                 
                 SpawnRandomPowerUp();
-                DebugPrint("Spawned a coin");
                 timeSinceCoin = 0;
+            }
+
+            if (timeSinceEnemy > cooldownEnemySpawn)
+            {
+
+                SpawnRandomEnemy();
+                timeSinceEnemy = 0;
             }
         }
     }
@@ -48,8 +58,16 @@ public class GameManager : MonoBehaviour
     private void SpawnRandomPowerUp()
     {
         int  index = Random.Range(0, powerUpPickupList.Length);
-        Vector3 coinVector = new Vector3(Random.Range(-45, 45), Random.Range(-5, 15), Random.Range(-45, 45));
+        Vector3 coinVector = new Vector3(Random.Range(-100, 100), Random.Range(-5, 15), Random.Range(-100, 100));
         PhotonNetwork.Instantiate(powerUpPickupList[index].name, coinVector, Quaternion.identity);
+
+    }
+
+    private void SpawnRandomEnemy()
+    {
+        int index = Random.Range(0, enemyList.Length);
+        Vector3 spawnVector = new Vector3(Random.Range(-100, 100), -9, Random.Range(-100, 100));
+        PhotonNetwork.Instantiate(enemyList[index].name, spawnVector, Quaternion.identity);
 
     }
 }
