740509764fa8f246d0b7ff2a4e893db7c5d1b06c
RasOtte <Rasmus.otterlind@gmail.com>
2022-05-20 10:03:09 +0200

    added sword

--- Assets/Scrpits/PowerUps/CoinShield/CoinShieldPowerUp.cs
+++ Assets/Scrpits/PowerUps/CoinShield/CoinShieldPowerUp.cs
@@ -11,6 +11,7 @@ public class CoinShieldPowerUp : PowerUp
     public static string key = "CoinShield";
     private PhotonView photonView;
     private int counter = 0;
+    private float offset = 5f;
     public override void AddAdditionalPowerUp()
     {
         if (photonView.IsMine && counter <= nUpgradesMax)
@@ -37,22 +38,27 @@ public class CoinShieldPowerUp : PowerUp
         coinShield.transform.SetParent(coinShieldTransform);
         if(counter == 0)
         {
-            coinShield.transform.localPosition = Vector3.forward * 3;
+            coinShield.transform.localPosition = Vector3.forward * offset + Vector3.up;
+            coinShield.transform.LookAt(transform.position + Vector3.up);
         }
         else if (counter == 1)
         {
-            coinShield.transform.localPosition = Vector3.forward * - 3;
+            coinShield.transform.localPosition = Vector3.forward * -offset + Vector3.up;
+            coinShield.transform.LookAt(transform.position + Vector3.up);
         }
         else if (counter == 2)
         {
-            coinShield.transform.localPosition = Vector3.right * 3;
+            coinShield.transform.localPosition = Vector3.right * offset + Vector3.up;
+            coinShield.transform.LookAt(transform.position + Vector3.up);
         }
         else if (counter == 3)
         {
-            coinShield.transform.localPosition = Vector3.right * -3;
+            coinShield.transform.localPosition = Vector3.right * -offset + Vector3.up;
+            coinShield.transform.LookAt(transform.position + Vector3.up);
+
         }
 
-        coinShield.transform.LookAt(coinShieldTransform);
+        coinShield.transform.LookAt(coinShield.transform.position - coinShield.transform.forward);
     }
     
     // Start is called before the first frame update
